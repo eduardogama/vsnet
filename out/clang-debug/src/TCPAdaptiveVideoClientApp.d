e2845d25c03c8f1fdcd4f4ad0572e7949205ff32
eduardo Gama <eduardogama72@gmail.com>
2019-03-22 19:11:52 -0300

    Updating ex1

--- out/clang-debug/src/TCPAdaptiveVideoClientApp.d
+++ out/clang-debug/src/TCPAdaptiveVideoClientApp.d
@@ -1,124 +1,123 @@
 ../out/clang-debug/src/TCPAdaptiveVideoClientApp.o: \
   TCPAdaptiveVideoClientApp.cc TCPAdaptiveVideoClientApp.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/INETDefs.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/precompiled_debug.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/precompiled.h \
+  ../../inet4/src/inet/common/INETDefs.h \
+  ../../inet4/src/inet/common/precompiled_debug.h \
+  ../../inet4/src/inet/common/precompiled.h \
   /home/futebol/omnetpp-5.4.1/include/omnetpp.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/opp_defines.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/features.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/Compat.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/InitStages.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/applications/tcpapp/TcpAppBase.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/applications/base/ApplicationBase.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/lifecycle/ModuleOperations.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/lifecycle/LifecycleOperation.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/lifecycle/OperationalBase.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/lifecycle/ILifecycle.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/transportlayer/contract/tcp/TcpSocket.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/Message.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/tag/TagSet.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/Packet.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/chunk/BitsChunk.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/chunk/Chunk.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/MemoryInputStream.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/Units.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/INETMath.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/linklayer/common/MacAddress.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/contract/ipv4/Ipv4Address.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/contract/ipv6/Ipv6Address.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/MemoryOutputStream.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/Ptr.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/IntrusivePtr.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/tag/RegionTagSet.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/chunk/BytesChunk.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/socket/ISocket.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/common/L3Address.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/common/ModuleIdAddress.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/common/ModulePathAddress.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/contract/clns/ClnsAddress.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/transportlayer/contract/tcp/TcpCommand_m.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/INETDefs_m.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/common/IpProtocolId_m.h \
-  /home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/common/L3Address_m.h
+  ../../inet4/src/inet/opp_defines.h ../../inet4/src/inet/features.h \
+  ../../inet4/src/inet/common/Compat.h \
+  ../../inet4/src/inet/common/InitStages.h \
+  ../../inet4/src/inet/applications/tcpapp/TcpAppBase.h \
+  ../../inet4/src/inet/applications/base/ApplicationBase.h \
+  ../../inet4/src/inet/common/lifecycle/ModuleOperations.h \
+  ../../inet4/src/inet/common/lifecycle/LifecycleOperation.h \
+  ../../inet4/src/inet/common/lifecycle/OperationalBase.h \
+  ../../inet4/src/inet/common/lifecycle/ILifecycle.h \
+  ../../inet4/src/inet/transportlayer/contract/tcp/TcpSocket.h \
+  ../../inet4/src/inet/common/packet/Message.h \
+  ../../inet4/src/inet/common/packet/tag/TagSet.h \
+  ../../inet4/src/inet/common/packet/Packet.h \
+  ../../inet4/src/inet/common/packet/chunk/BitsChunk.h \
+  ../../inet4/src/inet/common/packet/chunk/Chunk.h \
+  ../../inet4/src/inet/common/MemoryInputStream.h \
+  ../../inet4/src/inet/common/Units.h \
+  ../../inet4/src/inet/common/INETMath.h \
+  ../../inet4/src/inet/linklayer/common/MacAddress.h \
+  ../../inet4/src/inet/networklayer/contract/ipv4/Ipv4Address.h \
+  ../../inet4/src/inet/networklayer/contract/ipv6/Ipv6Address.h \
+  ../../inet4/src/inet/common/MemoryOutputStream.h \
+  ../../inet4/src/inet/common/Ptr.h \
+  ../../inet4/src/inet/common/IntrusivePtr.h \
+  ../../inet4/src/inet/common/packet/tag/RegionTagSet.h \
+  ../../inet4/src/inet/common/packet/chunk/BytesChunk.h \
+  ../../inet4/src/inet/common/socket/ISocket.h \
+  ../../inet4/src/inet/networklayer/common/L3Address.h \
+  ../../inet4/src/inet/networklayer/common/ModuleIdAddress.h \
+  ../../inet4/src/inet/networklayer/common/ModulePathAddress.h \
+  ../../inet4/src/inet/networklayer/contract/clns/ClnsAddress.h \
+  ../../inet4/src/inet/transportlayer/contract/tcp/TcpCommand_m.h \
+  ../../inet4/src/inet/common/INETDefs_m.h \
+  ../../inet4/src/inet/networklayer/common/IpProtocolId_m.h \
+  ../../inet4/src/inet/networklayer/common/L3Address_m.h
 
 TCPAdaptiveVideoClientApp.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/INETDefs.h:
+../../inet4/src/inet/common/INETDefs.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/precompiled_debug.h:
+../../inet4/src/inet/common/precompiled_debug.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/precompiled.h:
+../../inet4/src/inet/common/precompiled.h:
 
 /home/futebol/omnetpp-5.4.1/include/omnetpp.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/opp_defines.h:
+../../inet4/src/inet/opp_defines.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/features.h:
+../../inet4/src/inet/features.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/Compat.h:
+../../inet4/src/inet/common/Compat.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/InitStages.h:
+../../inet4/src/inet/common/InitStages.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/applications/tcpapp/TcpAppBase.h:
+../../inet4/src/inet/applications/tcpapp/TcpAppBase.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/applications/base/ApplicationBase.h:
+../../inet4/src/inet/applications/base/ApplicationBase.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/lifecycle/ModuleOperations.h:
+../../inet4/src/inet/common/lifecycle/ModuleOperations.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/lifecycle/LifecycleOperation.h:
+../../inet4/src/inet/common/lifecycle/LifecycleOperation.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/lifecycle/OperationalBase.h:
+../../inet4/src/inet/common/lifecycle/OperationalBase.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/lifecycle/ILifecycle.h:
+../../inet4/src/inet/common/lifecycle/ILifecycle.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/transportlayer/contract/tcp/TcpSocket.h:
+../../inet4/src/inet/transportlayer/contract/tcp/TcpSocket.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/Message.h:
+../../inet4/src/inet/common/packet/Message.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/tag/TagSet.h:
+../../inet4/src/inet/common/packet/tag/TagSet.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/Packet.h:
+../../inet4/src/inet/common/packet/Packet.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/chunk/BitsChunk.h:
+../../inet4/src/inet/common/packet/chunk/BitsChunk.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/chunk/Chunk.h:
+../../inet4/src/inet/common/packet/chunk/Chunk.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/MemoryInputStream.h:
+../../inet4/src/inet/common/MemoryInputStream.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/Units.h:
+../../inet4/src/inet/common/Units.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/INETMath.h:
+../../inet4/src/inet/common/INETMath.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/linklayer/common/MacAddress.h:
+../../inet4/src/inet/linklayer/common/MacAddress.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/contract/ipv4/Ipv4Address.h:
+../../inet4/src/inet/networklayer/contract/ipv4/Ipv4Address.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/contract/ipv6/Ipv6Address.h:
+../../inet4/src/inet/networklayer/contract/ipv6/Ipv6Address.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/MemoryOutputStream.h:
+../../inet4/src/inet/common/MemoryOutputStream.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/Ptr.h:
+../../inet4/src/inet/common/Ptr.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/IntrusivePtr.h:
+../../inet4/src/inet/common/IntrusivePtr.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/tag/RegionTagSet.h:
+../../inet4/src/inet/common/packet/tag/RegionTagSet.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/packet/chunk/BytesChunk.h:
+../../inet4/src/inet/common/packet/chunk/BytesChunk.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/socket/ISocket.h:
+../../inet4/src/inet/common/socket/ISocket.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/common/L3Address.h:
+../../inet4/src/inet/networklayer/common/L3Address.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/common/ModuleIdAddress.h:
+../../inet4/src/inet/networklayer/common/ModuleIdAddress.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/common/ModulePathAddress.h:
+../../inet4/src/inet/networklayer/common/ModulePathAddress.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/contract/clns/ClnsAddress.h:
+../../inet4/src/inet/networklayer/contract/clns/ClnsAddress.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/transportlayer/contract/tcp/TcpCommand_m.h:
+../../inet4/src/inet/transportlayer/contract/tcp/TcpCommand_m.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/common/INETDefs_m.h:
+../../inet4/src/inet/common/INETDefs_m.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/common/IpProtocolId_m.h:
+../../inet4/src/inet/networklayer/common/IpProtocolId_m.h:
 
-/home/futebol/omnetpp-5.4.1/samples/inet/src/inet/networklayer/common/L3Address_m.h:
+../../inet4/src/inet/networklayer/common/L3Address_m.h:
